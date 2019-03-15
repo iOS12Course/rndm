3a28b7202e33fd0d23f99bcbf7f420cf81a50d9f
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-03-14 21:02:54 -0500

    Comments VC Set up

---
+++ rndm/View/CommentCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentCell.swift
+//  rndm
+//
+//  Created by Ricardo Herrera Petit on 3/14/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
