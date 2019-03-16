54ad91d883bb9f3b593bbc7d6c8866d27f5259ab
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-03-15 23:42:38 -0500

    Security Rules: Project Setup

--- rndm/Model/Thought.swift
+++ rndm/Model/Thought.swift
@@ -17,14 +17,16 @@ class Thought {
     private (set) var numLikes: Int!
     private (set) var numComments: Int!
     private (set) var documentId: String!
+    private (set) var userId: String!
     
-    init(username:String, timestamp:Date, thoughtTxt:String, numLikes:Int, numComments:Int, documentId: String) {
+    init(username:String, timestamp:Date, thoughtTxt:String, numLikes:Int, numComments:Int, documentId: String, userId:String) {
         self.username = username
         self.timestamp = timestamp
         self.thoughTxt = thoughtTxt
         self.numLikes = numLikes
         self.numComments = numComments
         self.documentId = documentId
+        self.userId = userId
     }
     
     class func parseData(snapshot: QuerySnapshot?) -> [Thought] {
@@ -38,8 +40,9 @@ class Thought {
             let numLikes = data[NUM_LIKES] as? Int ?? 0
             let numComments = data[NUM_COMMENTS] as? Int ?? 0
             let documentId = document.documentID
+            let userId = data[USER_ID] as? String ?? ""
             
-            let thought = Thought(username: username, timestamp: timestamp, thoughtTxt: thoughtTxt, numLikes: numLikes, numComments: numComments, documentId: documentId)
+            let thought = Thought(username: username, timestamp: timestamp, thoughtTxt: thoughtTxt, numLikes: numLikes, numComments: numComments, documentId: documentId, userId: userId)
             thoughts.append(thought)
             
         }
